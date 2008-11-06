c3545091fcbb4ea8d7fe78efbf50a6a2d6f45656
Emmanuel Lacour <CENSORED>
2008-11-06 02:06:16 -0800

    import RT-Extension-WatchedQueues 0.03 from CPAN
    git-cpan-module:   RT-Extension-WatchedQueues
    git-cpan-version:  0.03
    git-cpan-authorid: ELACOUR
    git-cpan-file:     authors/id/E/EL/ELACOUR/RT-Extension-WatchedQueues-0.03.tar.gz

--- lib/RT/Extension/WatchedQueues.pm
+++ lib/RT/Extension/WatchedQueues.pm
@@ -9,11 +9,11 @@ RT::Extension::WatchedQueues - List queues the user is a watcher of
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
